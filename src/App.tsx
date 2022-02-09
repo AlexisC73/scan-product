ea7076644da664c4bb57f37d95f4e7d283b2c7e2
Alexis <alexis.comte73@icloud.com>
2022-02-09 11:15:21 +0100

    fix route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { BrowserRouter as Router ,Routes, Route} from 'react-router-dom'
-import Header from './components/Header';
-import ProductPage from './Page/Product';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import ProductPage from './Page/Product'
 import './styles/mains.scss'
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path='/' element={<Header />}/>
-        <Route path='/product/:barcode' element={<ProductPage />}/>
+        <Route path='/' element={<Header />} />
+        <Route path='/product/:barcode' element={<ProductPage />} />
+        <Route path='/product' element={<Header />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
